99e7b207e5921ff0fdf51424cc47339c4e5aa608
Guilherme Albino <guilhermealbino33@gmail.com>
2022-01-22 11:17:31 -0300

    Jest encountered an unexpected token

--- src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -1,3 +1,4 @@
+import { AppError } from "../../../../shared/errors/AppError";
 import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
 import { CreateUserUseCase } from "./CreateUserUseCase"
 
@@ -23,7 +24,27 @@ describe("Create user", () => {
       password: user.password,
     });
 
-    const userCreated = await inMemoryUsersRepository.findByEmail( user.email );
+    const userCreated = await inMemoryUsersRepository.findByEmail(user.email);
     expect(userCreated).toHaveProperty("id");
+  });
+  it("Should be able to create a new user with a existing email.", async () => {
+    expect(async () => {
+      const user = {
+        email: "username@email.com",
+        name: "username",
+        password: "1234",
+      };
+
+      await createUserUseCase.execute({
+        email: user.email,
+        name: user.name,
+        password: user.password,
+      });
+      await createUserUseCase.execute({
+        email: user.email,
+        name: user.name,
+        password: user.password,
+      });
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
