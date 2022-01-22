b06df714d7afc94518f7ed8b9ef2434651532e7a
Guilherme Albino <guilhermealbino33@gmail.com>
2022-01-22 10:50:07 -0300

    create user test

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -0,0 +1,29 @@
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "./CreateUserUseCase"
+
+let createUserUseCase: CreateUserUseCase;
+let inMemoryUsersRepository: InMemoryUsersRepository;
+
+describe("Create user", () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository);
+  });
+
+  it("Should be able to create a new user.", async () => {
+    const user = {
+      email: "username@email.com",
+      name: "username",
+      password: "1234",
+    };
+
+    await createUserUseCase.execute({
+      email: user.email,
+      name: user.name,
+      password: user.password,
+    });
+
+    const userCreated = await inMemoryUsersRepository.findByEmail( user.email );
+    expect(userCreated).toHaveProperty("id");
+  });
+});
