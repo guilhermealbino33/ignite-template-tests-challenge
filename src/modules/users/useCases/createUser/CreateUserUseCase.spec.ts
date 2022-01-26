665dc64e6b5aa99cf05d91288cbd8c3d3f118b0b
Guilherme Albino <guilhermealbino33@gmail.com>
2022-01-25 21:13:58 -0300

    users tests usecase

--- src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -1,6 +1,6 @@
 import { AppError } from "../../../../shared/errors/AppError";
 import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
-import { CreateUserUseCase } from "./CreateUserUseCase"
+import { CreateUserUseCase } from "./CreateUserUseCase";
 
 let createUserUseCase: CreateUserUseCase;
 let inMemoryUsersRepository: InMemoryUsersRepository;
@@ -12,22 +12,16 @@ describe("Create user", () => {
   });
 
   it("Should be able to create a new user.", async () => {
-    const user = {
+    const user = await createUserUseCase.execute({
       email: "username@email.com",
       name: "username",
       password: "1234",
-    };
-
-    await createUserUseCase.execute({
-      email: user.email,
-      name: user.name,
-      password: user.password,
     });
 
-    const userCreated = await inMemoryUsersRepository.findByEmail(user.email);
-    expect(userCreated).toHaveProperty("id");
+    expect(user).toHaveProperty("id");
   });
-  it("Should be able to create a new user with a existing email.", async () => {
+
+  it("Should not be able to create a new user with an existing email.", async () => {
     expect(async () => {
       const user = {
         email: "username@email.com",
