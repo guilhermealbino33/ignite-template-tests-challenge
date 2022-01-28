751db3d7eb2501e7b97ed3c12f3681aa2dab8050
Guilherme Albino <guilhermealbino33@gmail.com>
2022-01-28 16:01:59 -0300

    authenticate user

--- src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
+++ src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
@@ -1,5 +1,3 @@
-import "reflect-metadata";
-
 import { InMemoryUsersRepository } from "../../../users/repositories/in-memory/InMemoryUsersRepository";
 import { OperationType } from "../../entities/Statement";
 import { InMemoryStatementsRepository } from "../../repositories/in-memory/InMemoryStatementsRepository";
