1dab207a66da1e5297b7e467053efaea40fd7047
Guilherme Albino <guilhermealbino33@gmail.com>
2022-01-28 11:48:59 -0300

    Update CreateStatementUseCase.spec.ts

--- src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
+++ src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
@@ -3,16 +3,19 @@ import "reflect-metadata";
 import { InMemoryUsersRepository } from "../../../users/repositories/in-memory/InMemoryUsersRepository";
 import { OperationType } from "../../entities/Statement";
 import { InMemoryStatementsRepository } from "../../repositories/in-memory/InMemoryStatementsRepository";
+import { CreateStatementError } from "./CreateStatementError";
+import { CreateStatementUseCase } from "./CreateStatementUseCase";
 
 
 let inMemoryUsersRepository: InMemoryUsersRepository;
 let inMemoryStatementsRepository: InMemoryStatementsRepository;
-
+let createStatementUseCase: CreateStatementUseCase;
 
 describe("Create Statement", () => {
   beforeEach(() => {
     inMemoryUsersRepository = new InMemoryUsersRepository();
     inMemoryStatementsRepository = new InMemoryStatementsRepository();
+    createStatementUseCase = new CreateStatementUseCase(inMemoryUsersRepository, inMemoryStatementsRepository)
   });
   it("Should be able to create a new statement.", async () => {
     const user = await inMemoryUsersRepository.create({
@@ -29,7 +32,17 @@ describe("Create Statement", () => {
     });
 
     expect(statement).toHaveProperty("id");
-
+    expect(statement.user_id).toEqual(user.id);
   });
+  it("Should not be able to create a new statement without an existing user.", async () => {
+    await expect(async () => {
+      await createStatementUseCase.execute({
+        amount: 50,
+        description: "WITHDRAW",
+        type: OperationType.WITHDRAW,
+        user_id: "ANY"
+      });
+    }).rejects.toBeInstanceOf(CreateStatementError.UserNotFound);
+  })
 });
 
