82cdfb14dd636be6bc8f01ef2ab06b845ce0f0ed
Guilherme Albino <guilhermealbino33@gmail.com>
2022-04-11 21:25:22 -0300

    transfer module created

---
+++ src/modules/statements/useCases/createTransfer/CreateTransferController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { BalanceMap } from '../../mappers/BalanceMap';
+import { CreateTransferUseCase } from './CreateTransferUseCase';
+
+export class CreateTransferController {
+  async execute(request: Request, response: Response) {
+    const { id: user_id } = request.user;
+
+    const getBalance = container.resolve(CreateTransferUseCase);
+
+    const transfer = await getBalance.execute({ user_id });
+
+    const balanceDTO = BalanceMap.toDTO(transfer);
+
+    return response.json(balanceDTO);
+  }
+}
