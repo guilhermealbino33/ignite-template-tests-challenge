f1d14b708f0387fa947fd13da4eafd34f37847bc
Guilherme Albino <guilhermealbino33@gmail.com>
2022-04-26 22:00:26 -0300

    fix: indentation

---
+++ src/database/migrations/1651021006130-fin_api.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class finApi1651021006130 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
