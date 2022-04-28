3576b234d537bbdc74a21cb0fa4e9df37edcc956
Guilherme Albino <guilhermealbino33@gmail.com>
2022-04-28 09:28:51 -0300

    fix: password comparsion / secret as string

--- src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -1,8 +1,8 @@
 import { inject, injectable } from "tsyringe";
-import { compare } from 'bcryptjs';
-import { sign } from 'jsonwebtoken';
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
-import authConfig from '../../../../config/auth';
+import authConfig from "../../../../config/auth";
 
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { IAuthenticateUserResponseDTO } from "./IAuthenticateUserResponseDTO";
@@ -16,26 +16,27 @@ interface IRequest {
 @injectable()
 export class AuthenticateUserUseCase {
   constructor(
-    @inject('UsersRepository')
-    private usersRepository: IUsersRepository,
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
   ) {}
 
-  async execute({ email, password }: IRequest): Promise<IAuthenticateUserResponseDTO> {
+  async execute({
+    email,
+    password,
+  }: IRequest): Promise<IAuthenticateUserResponseDTO> {
     const user = await this.usersRepository.findByEmail(email);
 
-    if(!user) {
+    if (!user) {
       throw new IncorrectEmailOrPasswordError();
     }
 
-    const passwordMatch = await compare(password, user.password);
-
-    if (!passwordMatch) {
+    if (password !== user.password) {
       throw new IncorrectEmailOrPasswordError();
     }
 
     const { secret, expiresIn } = authConfig.jwt;
 
-    const token = sign({ user }, secret, {
+    const token = sign({ user }, String(secret), {
       subject: user.id,
       expiresIn,
     });
@@ -44,9 +45,9 @@ export class AuthenticateUserUseCase {
       user: {
         id: user.id,
         name: user.name,
-        email: user.email
+        email: user.email,
       },
-      token
-    }
+      token,
+    };
   }
 }
