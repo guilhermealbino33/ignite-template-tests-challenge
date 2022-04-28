5b06dfeaa7860912ae59ac091ad1b43bd3f0f503
Guilherme Albino <guilhermealbino33@gmail.com>
2022-04-28 08:49:11 -0300

    fix: indentation

--- src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -12,7 +12,9 @@ describe("Authenticate user", () => {
   beforeEach(() => {
     inMemoryUsersRepository = new InMemoryUsersRepository();
     createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository);
-    authenticateUserUseCase = new AuthenticateUserUseCase(inMemoryUsersRepository);
+    authenticateUserUseCase = new AuthenticateUserUseCase(
+      inMemoryUsersRepository
+    );
   });
 
   it("Should be able to authenticate a user", async () => {
@@ -27,49 +29,47 @@ describe("Authenticate user", () => {
 
     const session = await authenticateUserUseCase.execute({
       email: user.email,
-      password: "1234"
-    })
+      password: "1234",
+    });
     console.log("session", session);
-    expect(session).toHaveProperty("token")
-    expect(session).toHaveProperty("user")
+    expect(session).toHaveProperty("token");
+    expect(session).toHaveProperty("user");
 
-    expect(session.user).toHaveProperty("id")
-    expect(session.user.id).toBe(user.id)
-    expect(session.user).toHaveProperty("name")
-    expect(session.user.name).toBe(user.name)
-    expect(session.user).toHaveProperty("email")
-    expect(session.user.email).toBe(user.email)
+    expect(session.user).toHaveProperty("id");
+    expect(session.user.id).toBe(user.id);
+    expect(session.user).toHaveProperty("name");
+    expect(session.user.name).toBe(user.name);
+    expect(session.user).toHaveProperty("email");
+    expect(session.user.email).toBe(user.email);
   });
+});
+it("Should not be able to authenticate a wrong user password", async () => {
+  const password = await hash("1234", 8);
 
+  await inMemoryUsersRepository.create({
+    email: "username@email.com",
+    name: "username",
+    password: password,
   });
-  it("Should not be able to authenticate a wrong user password", async () => {
-    const password = await hash("1234", 8);
-
-    await inMemoryUsersRepository.create({
+  expect(async () => {
+    await authenticateUserUseCase.execute({
       email: "username@email.com",
-      name: "username",
-      password: password,
+      password: "4321",
     });
-    expect(async () => {
-      await authenticateUserUseCase.execute({
-        email: "username@email.com",
-        password: "4321",
-      });
-    }).rejects.toBeInstanceOf(IncorrectEmailOrPasswordError);
-  });
-  it("Should not be able to authenticate a wrong user e-mail", async () => {
-    const password = await hash("1234", 8);
+  }).rejects.toBeInstanceOf(IncorrectEmailOrPasswordError);
+});
+it("Should not be able to authenticate a wrong user e-mail", async () => {
+  const password = await hash("1234", 8);
 
-    await inMemoryUsersRepository.create({
-      email: "username@email.com",
-      name: "username",
-      password: password,
-    });
-    expect(async () => {
-      await authenticateUserUseCase.execute({
-        email: "wrong@email.com",
-        password: "1234",
-      });
-    }).rejects.toBeInstanceOf(IncorrectEmailOrPasswordError);
+  await inMemoryUsersRepository.create({
+    email: "username@email.com",
+    name: "username",
+    password: password,
   });
+  expect(async () => {
+    await authenticateUserUseCase.execute({
+      email: "wrong@email.com",
+      password: "1234",
+    });
+  }).rejects.toBeInstanceOf(IncorrectEmailOrPasswordError);
 });
