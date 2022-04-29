7f1dbeff3e46a601aac598014424546c4a8c5fdf
Guilherme Albino <guilhermealbino33@gmail.com>
2022-04-29 10:19:04 -0300

    fix: trying to fix transfer enumered

--- src/modules/statements/useCases/createTransfer/ITransferDTO.ts
+++ src/modules/statements/useCases/createTransfer/ITransferDTO.ts
@@ -1,6 +1,9 @@
+import { OperationType } from "../../entities/Statement";
+
 export interface ITransferDTO {
   payerId: string;
   beneficiaryID: string;
   amount: number;
   description: string;
+  type?: OperationType;
 }
